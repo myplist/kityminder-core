c11ee9bc8d12cca847f9a7bffdfbcd7e67cbf23f
晑驰 <robbenlee.lsp@alibaba-inc.com>
2017-09-05 18:30:06 +0800

    为图片增加缩放功能

--- src/module/image.js
+++ src/module/image.js
@@ -98,14 +98,21 @@ define(function(require, exports, module) {
             base: Renderer,
 
             create: function(node) {
-                return new kity.Image(node.getData('image'));
+                var group = new kity.Group().setId(utils.uuid('node_image'));
+                return group;
             },
 
             shouldRender: function(node) {
                 return node.getData('image');
             },
 
-            update: function(image, node, box) {
+            update: function(imageGroup, node, box) {
+                // 清除
+                var imageGroupLength = imageGroup.getItems().length;
+                while ( imageGroupLength > 0 ) {
+                    imageGroup.removeItem(--imageGroupLength);
+                }
+                var image = new kity.Image(node.getData('image'));
                 var url = node.getData('image');
                 var title = node.getData('imageTitle');
                 var size = node.getData('imageSize');
@@ -122,12 +129,97 @@ define(function(require, exports, module) {
 
                 image
                     .setUrl(url)
-                    .setX(x | 0)
-                    .setY(y | 0)
                     .setWidth(size.width | 0)
                     .setHeight(size.height | 0);
+                imageGroup.addShape(image);
+                var minusPath = 'M9.60 5.92H0.64V4.32h8.96v1.60z';
+                var minus = new kity.Path()
+                    .setTranslate(size.width + 4, 0)
+                    .setPathData(minusPath)
+                    .fill('white');
+                minus.on('click', function() {
+                    var height = node.getData('imageSize').height;
+                    var width = node.getData('imageSize').width;
+                    var ratio = height / width;
+                    if ( height > 200 ) {
+                        height -= 100;
+                        width = height / ratio;
+                        var imageSize = {
+                            height: height,
+                            width: width
+                        };
+                        node.setData('imageSize', imageSize);
+                        node.getMinder().refresh();
+                    }
+                });
+                minus.setStyle('cursor', 'pointer');
+                imageGroup.addShape(minus);
+                var plusPath = 'M10.24 4.38857143H5.85142857V0H4.38857143v4.38857143H0v1.46285714h4.38857143v4.38857143h1.46285714V5.85142857h4.38857143z';
+                var plus = new kity.Path()
+                    .setTranslate(size.width + 4, 18)
+                    .setPathData(plusPath)
+                    .fill('white');
+                plus.setStyle('cursor', 'pointer');
+                plus.on('click', function() {
+                    var height = node.getData('imageSize').height;
+                    var width = node.getData('imageSize').width;
+                    var ratio = height / width;
+                    if ( height < 500 ) {
+                        height += 100;
+                        width = height / ratio;
+                        var imageSize = {
+                            height: height,
+                            width: width
+                        };
+                        node.setData('imageSize', imageSize);
+                        node.getMinder().refresh();
+                    }
+                });
+                imageGroup.addShape(plus);
 
-                return new kity.Box(x | 0, y | 0, size.width | 0, size.height | 0);
+                var imageUrl = node.getData('image');
+                if ( imageUrl && imageUrl.indexOf('restapi.amap.com') !== -1 ) {
+                    var zoomInPath = 'M8.7,6.2H5.3C4.8,6.2,4.5,6.6,4.5,7c0,0,0,0,0,0c0,0.4,0.3,0.8,0.8,0.8h3.5c0.4,0,0.8-0.3,0.8-0.8C9.5,6.6,9.2,6.2,8.7,6.2zM14.6,13.5l-3.2-3.2c1.8-2.4,1.4-5.9-1.1-7.7S4.4,1.2,2.5,3.6s-1.4,5.9,1.1,7.7c1,0.7,2.1,1.1,3.3,1.1c1.2,0,2.4-0.4,3.3-1.1l3.2,3.2c0.3,0.3,0.7,0.3,0.9,0c0,0,0,0,0,0l0.1-0.1C14.8,14.2,14.8,13.8,14.6,13.5z M6.9,10.9c-2.2,0-4-1.8-4-4s1.8-4,4-4s4,1.8,4,4S9.1,10.9,6.9,10.9z';
+                    var zoomIn = new kity.Path()
+                        .setTranslate(size.width + 3, 36)
+                        .setPathData(zoomInPath)
+                        .fill('white');
+                    zoomIn.setStyle('cursor', 'pointer');
+                    zoomIn.on('click', function() {
+                        var zoomRegex = /.*(zoom=\d{1,2}).*/g;
+                        var zoomKV = zoomRegex.exec(node.getData('image'))[1];
+                        var zoomValue = +zoomKV.split('=')[1];
+
+                        if ( zoomValue > 1 ) {
+                            zoomValue -= 1;
+                            node.setData('image', node.getData('image').replace(zoomKV, 'zoom=' + zoomValue));
+                            node.getMinder().refresh();
+                        }
+                    });
+                    imageGroup.addShape(zoomIn);
+
+                    var zoomOutPath = 'M8,5.7C7.6,5.7,7.2,6,7.2,6.4S7.6,7.2,8,7.2h0.8V8c0,0.4,0.3,0.8,0.8,0.8s0.8-0.3,0.8-0.8V7.2h0.8c0.4,0,0.8-0.3,0.8-0.8s-0.3-0.8-0.8-0.8h-0.8V4.9c0-0.4-0.3-0.8-0.8-0.8S8.8,4.5,8.8,4.9v0.8L8,5.7z M1.4,12.9c-0.5,0.5-0.5,1.3,0,1.7c0.5,0.5,1.3,0.5,1.7,0l3.6-3.6c0.8,0.5,1.8,0.8,2.9,0.8c3,0,5.4-2.4,5.4-5.4c0-3-2.4-5.4-5.4-5.4S4.1,3.4,4.1,6.4c0,1.1,0.3,2,0.8,2.9L1.4,12.9z M5.7,6.4c0-2.1,1.7-3.9,3.9-3.9s3.9,1.7,3.9,3.9s-1.7,3.9-3.9,3.9S5.7,8.6,5.7,6.4z';
+                    var zoomOut = new kity.Path()
+                        .setTranslate(size.width, 56)
+                        .setPathData(zoomOutPath)
+                        .fill('white');
+                    zoomOut.setStyle('cursor', 'pointer');
+                    zoomOut.on('click', function() {
+                        var zoomRegex = /.*(zoom=\d{1,2}).*/g;
+                        var zoomKV = zoomRegex.exec(node.getData('image'))[1];
+                        var zoomValue = +zoomKV.split('=')[1];
+
+                        if ( zoomValue < 17 ) {
+                            zoomValue += 1;
+                            node.setData('image', node.getData('image').replace(zoomKV, 'zoom=' + zoomValue));
+                            node.getMinder().refresh();
+                        }
+                    });
+                    imageGroup.addShape(zoomOut);
+                }
+
+                imageGroup.setTranslate(x, y);
+                return new kity.Box(x | 0, y | 0, size.width + 8| 0, size.height | 0);
             }
         });
 
